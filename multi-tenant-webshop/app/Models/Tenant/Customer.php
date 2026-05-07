bbc52e5d81e3775d6c9dea4a5d52e72167e63d46
Kirano De Meester <kiranodemeester@gmail.com>
2026-05-07 10:14:56 +0200

    tenant & customer aut

---
+++ multi-tenant-webshop/app/Models/Tenant/Customer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models\Tenant;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
+class Customer extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $connection = 'tenant';
+
+    protected $guarded = [];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+}
